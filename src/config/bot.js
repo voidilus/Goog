1d1ffccd1a9bb432b53a673906fca2eb631e57d0
voidilus <voidilus@gmail.com>
2026-09-16 19:45:11 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "Boiling Point", // required by Discord API, not shown in the client
         state: "Boiling Point is so PEAK",     // this is what people actually see
-        type: 3,               // Custom
+        type: 2,               // Custom
       },
     ],
   },
